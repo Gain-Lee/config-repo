f7fcd2955b7fde3a5cc9cf8929d24d2c92c39c05
GAIN LEE <dlrkdls91@naver.com>
2018-09-28 16:42:21 +0900

    Spring cloud microservice licensing server

---
+++ licensing-server/src/main/java/com/handycar/licensingserver/LicensingServerApplication.java
@@ -0,0 +1,12 @@
+package com.handycar.licensingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensingServerApplication.class, args);
+    }
+}
---
+++ licensing-server/src/main/java/com/handycar/licensingserver/services/LicenseService.java
@@ -0,0 +1,45 @@
+package com.handycar.licensingserver.services;
+
+import com.handycar.licensingserver.config.ServiceConfig;
+import com.handycar.licensingserver.model.License;
+import com.handycar.licensingserver.repository.LicenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class LicenseService {
+
+    @Autowired
+    private LicenseRepository licenseRepository;
+
+    @Autowired
+    ServiceConfig config;
+
+    public License getLicense(String organizationId,String licenseId) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        return license.withComment(config.getExampleProperty());
+    }
+
+    public List<License> getLicensesByOrg(String organizationId){
+        return licenseRepository.findByOrganizationId( organizationId );
+    }
+
+    public void saveLicense(License license){
+        license.withId( UUID.randomUUID().toString());
+
+        licenseRepository.save(license);
+
+    }
+
+    public void updateLicense(License license){
+      licenseRepository.save(license);
+    }
+
+    public void deleteLicense(License license){
+        licenseRepository.deleteById( license.getLicenseId());
+    }
+
+}
